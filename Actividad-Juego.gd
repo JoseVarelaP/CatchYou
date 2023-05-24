bf35f78362713f9eb9568dc23ae029b60b02ce5d
Jose_Varela <jose@josevarela.net>
2023-05-23 22:30:27 -0600

    Agrega opción para reiniciar

--- Actividad-Juego.gd
+++ Actividad-Juego.gd
@@ -16,6 +16,15 @@ func ProcesaTiempo(sec):
 		return "%02d:%02d:%02d" % [hours, minutes, seconds]
 	
 	return "%02d:%02d" % [minutes, seconds]
+	
+func actionLoseGame():
+	# Marca el juego como terminado, para evitar que el jugador y los elementos sean
+	# actualizados.
+	lostGame = true
+	$Jugador.setAllowedToMove(false)
+	
+	# Muestra la interfaz.
+	$"Interfaz-Perdiste".set_visible(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,5 +39,12 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	print("weeee")
 	if( body == $Jugador ):
-		lostGame = true
-		$Jugador.setAllowedToMove(false)
+		actionLoseGame()
+
+
+func _on_reiniciar_button_up():
+	# Recarga los elementos y reinicia la partida.
+	get_tree().reload_current_scene()
+
+func _on_salir_button_up():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
